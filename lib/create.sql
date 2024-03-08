40e9884592982f73a5e1c16ce20bda77e63409db
Allanshikomere <allanshikomere@gmail.com>
2024-03-08 16:16:56 +0300

    Completed

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+  //columns here
+  id INTERGER PRIMARY KEY
+  name TEXT
+  age INTERGER
+  sex TEXT
+  color TEXT
+  temperature TEXT
+  alive BOOLEAN
+);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,29 @@
+# Query to select all female bears and return their name and age
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE sex = 'F';
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+# Query to select the oldest bear's name and age
+select_oldest_bears_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age DESC
+    LIMIT 1;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+# Query to select the count of bears by sex
+select_count_of_bears_by_sex = """
+    SELECT
+        sex,
+        COUNT(*) AS count
+    FROM bears
+    GROUP BY sex;
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
+
